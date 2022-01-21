e41c7b5aec281f5cd8e9de22f6f2e992ee28a697
wwojtas <wojciech.wus.programista@gmail.com>
2022-01-21 11:44:28 +0100

    this

---
+++ src/KlasyThis/KlasaPodstawowa.java
@@ -0,0 +1,15 @@
+package KlasyThis;
+
+public class KlasaPodstawowa {
+
+    KlasaPodstawowa() {
+        System.out.println("Konstruktor z klasa podstawowa");
+    }
+
+    KlasaPodstawowa(int a) {
+        System.out.println("Konstruktor z klasa podstawowa z parametrem" + a);
+    }
+
+
+
+}
