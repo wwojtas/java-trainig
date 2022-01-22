576cfe60c1930a9ab867fe92ac3029fe66d185bd
wwojtas <wojciech.wus.programista@gmail.com>
2022-01-22 21:45:34 +0100

    Klasa Thread - watki

---
+++ src/Watki/MojDrugiWatek.java
@@ -0,0 +1,9 @@
+package Watki;
+
+public class MojDrugiWatek extends Thread{
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("Moj drugi watek: " + i);
+        }
+    }
+}
---
+++ src/Watki/MojPierwszyWatek.java
@@ -0,0 +1,14 @@
+package Watki;
+
+public class MojPierwszyWatek extends Thread{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("Moj pierwszy watek: " + i);
+        }
+    }
+
+
+
+}
