dfe61cf941ec44b9de5dedce56b06c278718b402
wwojtas <wojciech.wus.programista@gmail.com>
2022-01-24 09:07:23 +0100

    abstrakcja

---
+++ src/ProgramowanieObiektowe/abstrakcja/MainAbstrakcja.java
@@ -0,0 +1,9 @@
+package ProgramowanieObiektowe.abstrakcja;
+
+public class MainAbstrakcja {
+    public static void main(String[] args) {
+
+        Osoba osoba = new Osoba("Jan", "Kowalski");
+
+    }
+}
